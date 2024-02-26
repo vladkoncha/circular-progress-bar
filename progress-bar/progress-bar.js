5a28df2f359ece6fa0f96144a597ba4d2e6e1f81
Vlad <vla.koncha@gmail.com>
2024-02-26 12:22:40 +0700

    add progress bar api

--- progress-bar/progress-bar.js
+++ progress-bar/progress-bar.js
@@ -1,1 +1,66 @@
-const valueCircle = document.querySelector(".valueCircle");
+// Must be the same as value-circle's stroke-dasharray and pathLength
+const MAX_VALUE = 100;
+
+export class ProgressBar {
+  #progressBarElement = null;
+  #valueCircle = null;
+  #state = { value: 50, animated: false, hidden: false };
+
+  constructor(progressBarElement, valueCircle, params) {
+    this.#progressBarElement = progressBarElement;
+    this.#valueCircle = valueCircle;
+    this.#state = { ...this.#state, ...params };
+
+    this.#initProgressBar();
+  }
+
+  #initProgressBar() {
+    this.setValue(this.#state.value);
+
+    if (this.#state.animated) {
+      this.startAnimation();
+    } else {
+      this.stopAnimation();
+    }
+
+    if (this.#state.hidden) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+
+  setValue(value) {
+    this.#state.value = value;
+
+    this.#valueCircle.setAttribute(
+      "stroke-dashoffset",
+      MAX_VALUE - this.#state.value
+    );
+
+    if (this.#state.value === MAX_VALUE) {
+      this.#valueCircle.setAttribute("stroke-dasharray", "none");
+    } else {
+      this.#valueCircle.setAttribute("stroke-dasharray", MAX_VALUE);
+    }
+  }
+
+  startAnimation() {
+    this.#valueCircle.style.animation =
+      "rotate-progress-bar 2s linear infinite";
+  }
+
+  stopAnimation() {
+    this.#valueCircle.style.animation = "";
+  }
+
+  show() {
+    this.#state.hidden = false;
+    this.#progressBarElement.style.visibility = "visible";
+  }
+
+  hide() {
+    this.#state.hidden = true;
+    this.#progressBarElement.style.visibility = "hidden";
+  }
+}
---
+++ script.js
@@ -0,0 +1,32 @@
+import { ProgressBar } from "./progress-bar/progress-bar.js";
+
+const progressValueInput = document.querySelector(".progress-value-input");
+const animationSwitch = document.querySelector(".animate-switch");
+const visibilitySwitch = document.querySelector(".hide-switch");
+
+const progressBarElement = document.querySelector(".circular-bar-container");
+const valueCircle = document.querySelector(".value-circle");
+
+const progressBar = new ProgressBar(progressBarElement, valueCircle, {
+  value: +progressValueInput.value,
+  animated: animationSwitch.checked,
+  hidden: visibilitySwitch.checked,
+});
+
+const handleValueInput = () => {
+  progressBar.setValue(+progressValueInput.value);
+};
+
+const handleAnimationToggle = () => {
+  animationSwitch.checked
+    ? progressBar.startAnimation()
+    : progressBar.stopAnimation();
+};
+
+const handleVisibilityToggle = () => {
+  visibilitySwitch.checked ? progressBar.hide() : progressBar.show();
+};
+
+progressValueInput.addEventListener("input", handleValueInput);
+animationSwitch.addEventListener("input", handleAnimationToggle);
+visibilitySwitch.addEventListener("input", handleVisibilityToggle);
